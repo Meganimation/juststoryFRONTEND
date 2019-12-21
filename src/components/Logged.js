de3d93d65f443de62f959ded01fc4c7c454f0f16
Megan <MeganOliviaWendyAdams@gmail.com>
2019-12-21 15:04:01 -0500

    implementing oAuth 2

--- src/components/Logged.js
+++ src/components/Logged.js
@@ -2,19 +2,73 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage'
 import {Link} from 'react-router-dom'
+import FacebookLogin from 'react-facebook-login';
+import MainContainer from './MainContainer'
 
 
 class Logged extends Component {
 
 
+state = {
+    isLoggedIn: false,
+    userID: '',
+    name: '',
+    email: '',
+    picture: ''
+}
+responseFacebook = response => {
+    console.log(response)
+}
 
-
-
+componentClicked = () => {
+    console.log('clicked!')
+}
     
 render() {
+
+
+let fbContent;
+
+if(this.state.isLoggedIn) {
+   
+        
+    fbContent = 
+    <div> 
+    <MainContainer id={window.localStorage.id} name={window.localStorage.name} email={this.state.email} facebook_id={this.state.userID}     />      
+    
+
+    </div>;
+
+} else {
+    fbContent = ( 
+    <div className = "myMainBox" >
+        
+
+    <br/><br/>
+
+    
+    <FacebookLogin
+    appId="1378656285636593"
+    autoLoad={true}
+    fields="name,email,picture"
+    onClick={this.componentClicked}
+    callback={this.responseFacebook} /> 
+    
+
+    <br/>
+    <br/>
+
+
+    </div> )}
+
+
+
+
+
     return ( 
     <>
     <div className="form-group">
+   
     <form> 
 
         <br/>
@@ -41,8 +95,11 @@ render() {
         <br/>
         <br/>
 
-    <button className="myButton" 
-    onClick={this.props.doSomething}>I don't have an account!</button>
+    {/* <button className="myButton" 
+    onClick={this.props.doSomething}>I don't have an account!</button> */}
+    
+     {fbContent} 
+    {/* the button for facebook login */}
 </form>
 </div>
 </>  
